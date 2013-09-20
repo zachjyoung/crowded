ff89c24cbc1c0b007e03f732068e9879c0c9c7fb
Zach Young <zachjyoung@gmail.com>
2013-09-20 17:03:42 -0400

    installed all the rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowded::Application.config.secret_key_base = '3d5c29928d7a23cec9ec2ce3082ccc185f30514052d2a4f2d9a987b1597761447f5999ea0e7d5ae128940c6347ccfaae59ff3ef84e10ad4dd61f08f061bdfa75'
